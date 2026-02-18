99c0230dcaa573fc30d61bc3d04ac16a9c3d3a5f
isaac S. Silva <85599926+IsaacSSilva@users.noreply.github.com>
2026-02-17 22:44:44 -0300

    type confg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config: { experiments: any; }) => {
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
